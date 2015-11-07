24a149443ffd4dc81b2cb22a9cf52d1565b7a5de
gjack <gabi@gabijack.com>
2015-11-07 15:17:35 -0800

    Changing default devise error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+    
+    html = <<-HTML
+    <div class="alert alert-error alert-danger">
+      <a href='#' class='close' data-dismiss='alert' &#215;></a>
+      <%= content_tag :div, msg is msg is_a?(String) %>
+    </div>
+    HTML
+    
+    html.html_safe
+  end
 end
