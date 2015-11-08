7a28386519358fa5c7da47875466140a15c57f68
gjack <gabi@gabijack.com>
2015-11-07 22:03:16 -0800

    Create exercises successfully

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root to: 'dashboard#index'
-
+   
+   resources :users do
+     resources :exercises
+   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
